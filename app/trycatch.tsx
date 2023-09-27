018f62993648e2d252269139d80e44a1c0ce38ef
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-27 12:53:31 +1000

    Remove unnecesary code

--- app/trycatch.tsx
+++ app/trycatch.tsx
@@ -6,30 +6,12 @@ import { Suspense } from "react"
  * 1. Server tries to render component
  * 2. Server fails rendering component
  * 3. Error gets caught by catch()
- * 4. Renders fallback on the client with retry button
- * 5. Client comps renders fallback first with button
- * 
+ * 4. Renders fallback on the client
+ * 5. Client comps renders fallback, error message retrieved from useError
  */
-
-
-
 
 export async function TryCatch<P>({ Check, onError, ...rest }: {
   Check: React.FunctionComponent<P>,
-  // children?: React.ReactNode,
-  onError: (error: any) => (React.ReactNode | Promise<React.ReactNode>),
-} & P) {
-
-  return (
-    // <Suspense>
-      <TryCatchInternal Check={ Check } onError={ onError } {...rest } />
-    // </Suspense>
-  )
-
-}
-
-async function TryCatchInternal<P>({ Check, onError, ...rest }: {
-  Check: React.FunctionComponent<any>,
   // children?: React.ReactNode,
   onError: (error: any) => (React.ReactNode | Promise<React.ReactNode>),
 } & P) {
@@ -49,3 +31,4 @@ async function TryCatchInternal<P>({ Check, onError, ...rest }: {
   }
 
 }
+
